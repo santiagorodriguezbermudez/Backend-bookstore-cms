8f311776860fe638fffe18374632535ab9f5ffb4
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-10-15 15:32:00 -0500

    Added ngrok for development hosting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
-  config.hosts << "2c805e7a563e.ngrok.io"
+  config.hosts << "eaf4a741d721.ngrok.io"
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
